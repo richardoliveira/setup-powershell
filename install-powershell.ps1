05be8a1271cf5535072203215f50ea400d818820
Richard Oliveira <richardcorp@hotmail.com>
2024-10-06 06:37:53 -0300

    Update install-powershell.ps1

--- install-powershell.ps1
+++ install-powershell.ps1
@@ -1,4 +1,5 @@
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force
 winget install JanDeDobbeleer.OhMyPosh -s winget
 Add-Content -Path $PROFILE -Value "`noh-my-posh init pwsh | Invoke-Expression"
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 Write-Host "Oh My Posh foi instalado e configurado. Por favor, reinicie o PowerShell para aplicar as mudanças."
