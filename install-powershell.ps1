796cc390709d060060ba4df28ea910a38f7ac5c6
Richard Oliveira <richardcorp@hotmail.com>
2024-10-08 09:09:36 -0300

    .

--- install-powershell.ps1
+++ install-powershell.ps1
@@ -13,22 +13,35 @@ function Install-Font {
         [string]$url,
         [string]$fontName
     )
-    $fontPath = "$env:TEMP\$fontName"
-    Invoke-WebRequest -Uri $url -OutFile $fontPath
-    Start-Process $fontPath -ArgumentList '/VERYSILENT' -Wait
-    Remove-Item $fontPath
+
+    $fontZipPath = "$env:TEMP\$fontName.zip"
+    Invoke-WebRequest -Uri $url -OutFile $fontZipPath
+
+    # Extrair o arquivo zip
+    $extractPath = "$env:TEMP\$fontName"
+    Expand-Archive -Path $fontZipPath -DestinationPath $extractPath -Force
+
+    # Instalar as fontes
+    Get-ChildItem "$extractPath\*.ttf" | ForEach-Object {
+        $fontFile = $_.FullName
+        $fontInstaller = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+        Start-Process -FilePath $fontInstaller -ArgumentList "Add-Type -AssemblyName System.Drawing; [System.Drawing.Text.PrivateFontCollection]::new().AddFontFile('$fontFile')"
+    }
+
+    # Limpar arquivos temporários
+    Remove-Item -Path $fontZipPath
+    Remove-Item -Path $extractPath -Recurse -Force
 }
 
 $fonts = @{
-    "Meslo" = "https://github.com/andreberg/Meslo-Font/blob/master/dist/v1.2/MesloLGS%20NF%20Regular.ttf?raw=true";
+    "Meslo" = "https://github.com/andreberg/Meslo-Font/raw/refs/heads/master/dist/v1.2.1/Meslo%20LG%20v1.2.1.zip";
     "Fira Code" = "https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip";
     "Inter" = "https://github.com/rsms/inter/releases/download/v3.19/Inter-3.19.zip";
     "JetBrains Mono" = "https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip"
 }
 
 foreach ($font in $fonts.GetEnumerator()) {
-    Install-Font -url $font.Value -fontName "$($font.Key).exe"
+    Install-Font -url $font.Value -fontName $font.Key
 }
 
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 Write-Host "Oh My Posh e as fontes foram instalados e configurados. Por favor, reinicie o PowerShell para aplicar as mudanças."
