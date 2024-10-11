704731eee0c093cb07ac78e1ae36a638a11240a5
Richard Oliveira <richardcorp@hotmail.com>
2024-10-11 11:13:20 -0300

    Update install-powershell.ps1

--- install-powershell.ps1
+++ install-powershell.ps1
@@ -1,5 +1,7 @@
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force
 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
